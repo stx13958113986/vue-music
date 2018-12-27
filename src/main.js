a896f395d4a8f23e88c8d4086ea05de43afe04f7
stx13958113986 <937183394@qq.com>
2018-12-27 15:49:13 +0800

    12.27

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import 'common/stylus/index.styl'
+import 'babel-polyfill'
+import fastClick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Rank from 'components/rank/rank'
+import Recommend from 'components/recommend/recommend'
+import Singer from 'components/singer/singer'
+import Search from 'components/search/search'
+import SingerDetail from 'components/singer-detail/singer-detail'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/rank',
+      component: Rank
+    },
+    {
+      path: '/singer',
+      component: Singer,
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
+    },
+    {
+      path: '/search',
+      component: Search
     }
   ]
 })
